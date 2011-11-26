a7f327b45b9b4f2f56dbef11a4bd575862d348ac
Jan Gloser <jan.gloser@centrum.cz>
2011-11-26 19:09:12 +0100

    Refactored the animating code to a class. Still need to create instances somehow.

--- app/assets/javascripts/animator.js
+++ app/assets/javascripts/animator.js
@@ -1,35 +1,31 @@
-//How can I do this better in javascript?
-var speed_modifier = 0.15;
+//TODO : Create instances so that the behaviour is separated and can be used for different objects in the site
+Animator = {
+    animate : function(){
+	var subject = $('.dancer');
+	for(var i = 0; i < subject.length; i++){
+	    var width = parseInt(subject[i].style.width) || 0;
+	    var height = parseInt(subject[i].style.height) || 0;
+	    var max_x = (width == 0) ? 100 : (Math.floor(100-100*width/window.innerWidth));
+	    var max_y = (height == 0) ? 100 : (Math.floor(100-100*height/window.innerHeight));
+	    
+	    subject[i].style.left = Math.random()*max_x + '%';
+	    subject[i].style.top = Math.random()*max_y + '%';
+	}
+	//requestAnimFrame(animate, subject);
+    },
 
-function animate(){
-    console.log('Re-rendering');
-    var subject = $('.dancer');
-    for(var i = 0; i < subject.length; i++){
-	var width = parseInt(subject[i].style.width) || 0;
-	var height = parseInt(subject[i].style.height) || 0;
-	var max_x = (width == 0) ? 100 : (Math.floor(100-100*width/window.innerWidth));
-	var max_y = (height == 0) ? 100 : (Math.floor(100-100*height/window.innerHeight));
-	var coor_x = parseInt(subject[i].style.left) || 0;
-	var coor_y = parseInt(subject[i].style.top) || 0;
+    animation_loop : function(){
+	//Can 'this' be used here somehow?
+	var trigger = $('button.trigger#basic-trigger');
+	trigger.attr('onclick', 'Animator.kill_animation();');
+	trigger.text('Click me to stop animating');
+	this.interval_id = setInterval(this.animate, 1000);
+    },
 
-	console.log('width ' + width);
-	console.log('height ' + height);
-	console.log('window height ' + window.innerHeight);
-	console.log('max_x ' + max_x);
-	console.log('max_y ' + max_y);
-
-	coor_x = Math.random()*max_x + '%';
-	coor_y = Math.random()*max_y + '%';
-
-	console.log(coor_x);
-	console.log(coor_y);
-	
-	subject[i].style.left = coor_x;
-	subject[i].style.top = coor_y;
+    kill_animation : function(){
+	var trigger = $('button.trigger#basic-trigger');
+	trigger.attr('onclick', 'Animator.animation_loop();');
+	trigger.text('Click me to start animating');
+	clearInterval(this.interval_id);	
     }
-    //requestAnimFrame(animate, subject);
-}
-
-function animation_loop(){
-    setInterval(animate, 1000);
 }
