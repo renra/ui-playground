3e9795addb4566fb141d77ed70add460907f830f
Jan Gloser <jan.gloser@centrum.cz>
2011-11-26 11:37:58 +0100

    Switched to a rails project. Will be easier

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UiPlayground::Application.config.secret_token = 'b5d3954179f7c9ba9a252b944371a19ce9b14c31be2a14e07593d32ac9e5f927b1c6453f66c69de368265662abb56454a589249d18361298d9489cc96f37cb6e'
