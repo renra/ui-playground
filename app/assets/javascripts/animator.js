92908b5691a3fcdc154930c60720cb2a64575ead
Jan Gloser <jan.gloser@centrum.cz>
2011-11-28 00:45:43 +0100

    Preparing the ground for multiple animation

--- app/assets/javascripts/animator.js
+++ app/assets/javascripts/animator.js
@@ -10,83 +10,106 @@ window.requestAnimFrame = (function(){
 	
 })();
 
-//TODO : Create instances so that the behaviour is separated and can be used for different objects on the site
 Animator = {
-    init : function(){
-	var trigger = $('#basic-trigger');
-	var subject = $('.dancer');
-	this.x = subject.position().left;
-	this.y = subject.position().top;
-	this.subject = subject[0];
-	
-	this.check_drawing_area();
-	this.new_destination();
-	trigger.unbind('click');
-	trigger.click(function(){Animator.kill_animation()});
-	trigger.text('Click me to stop animating');
-	this.go_on = true;
-	this.animate();
-    },
-
-    go_on : true,
-    speed : 0.1,
+    master_kill : false,
+    master_speed : 0.1,
     max_x : 0,
     max_y : 0,
+    //put all the dancers here
+    dancers : [],
+    
+    init : function(){
+	//Finds four divs?? Confusing
+	var dancers = $(this).parent().parent().find('.dancer');
+	var jquery_wrapper = null;
+	var index = null;
+	console.log(dancers);
+	for(var i in dancers){
+	    console.log(dancers[i]);
+	    jquery_wrapper = $(dancers[i]);
+	    index = Animator.dancers.length;
+	    Animator.dancers[index] = {
+		element : dancers[i],
+		pos_x : jquery_wrapper.position().left,
+		pos_y : jquery_wrapper.position().top,
+		speed : Animator.master_speed,
+		go_on : true
+	    }
+
+	    Animator.delimit_dancing_area_for(index);
+	    Animator.set_destination_for(index);
+	}
+
+	console.log('After the loop');
+	var trigger = $('#basic-trigger');
+	trigger.unbind('click');
+	trigger.click(Animator.kill_animation);
+	trigger.text('Click me to stop animating');
+
+	console.log('About to animate');
+	Animator.animate();
+    },
     
     animate : function(){
-	if(!Animator.go_on){
+	if(Animator.master_kill){
 	    return;
 	}
 
-	Animator.check_drawing_area();
+	var flag = null;
+	var dancer = null;
+	for(var i in Animator.dancers){
+	    dancer = Animator.dancers[i];
+	    Animator.delimit_dancing_area_for(i);
 
-	var flag = 0;
-	if( (Math.abs(Animator.dest_x - Animator.x)) > 1 ){
-	    Animator.x += (Animator.dest_x - Animator.x)*Animator.speed;
-	    Animator.subject.style.left = Animator.x + 'px';
-	}
-	else{
-	    flag++;
-	}
+	    flag = 0;
+	    if( (Math.abs(dancer.dest_x - dancer.pos_x)) > 1 ){
+		dancer.pos_x += (dancer.dest_x - dancer.pos_x)*dancer.speed;
+		dancer.element.style.left = dancer.pos_x + 'px';
+	    }
+	    else{
+		flag++;
+	    }
 
-	if( (Math.abs(Animator.dest_y - Animator.y)) > 1 ){
-	    Animator.y += (Animator.dest_y - Animator.y)*Animator.speed;
-	    Animator.subject.style.top = Animator.y + 'px';
-	}
-	else{
-	    flag++;
-	}
+	    if( (Math.abs(dancer.dest_y - dancer.pos_y)) > 1 ){
+		dancer.pos_y += (dancer.dest_y - dancer.pos_y)*dancer.speed;
+		dancer.element.style.top = dancer.pos_y + 'px';
+	    }
+	    else{
+		flag++;
+	    }
 
-	if(flag >= 2){
-	    Animator.new_destination();
+	    console.log('Here');
+	    if(flag >= 2){
+		Animator.set_destination_for(i);
+	    }
 	}
 	
-	requestAnimFrame(Animator.animate, Animator.subject);
+//	requestAnimFrame(Animator.animate, Animator.dancers);
     },
 
-    new_destination : function(){
-	Animator.dest_x = Math.random()*Animator.max_x;
-	Animator.dest_y = Math.random()*Animator.max_y;
+    delimit_dancing_area_for : function(index){
+	var subject = Animator.dancers[index];
+	var width = parseInt(subject.element.style.width) || 0;
+	var height = parseInt(subject.element.style.height) || 0;
+	subject.max_x = Math.floor(window.innerWidth - width);
+	subject.max_y = Math.floor(window.innerHeight - height);
     },
 
-    check_drawing_area : function(){
-	var width = parseInt(Animator.subject.style.width) || 0;
-	var height = parseInt(Animator.subject.style.height) || 0;
-	Animator.max_x = Math.floor(window.innerWidth - width);
-	Animator.max_y = Math.floor(window.innerHeight - height);
+    set_destination_for : function(index){
+	var subject = Animator.dancers[index];
+	subject.dest_x = Math.random()*subject.max_x;
+	subject.dest_y = Math.random()*subject.max_y;
     },
 
     kill_animation : function(){
 	var trigger = $('button.trigger#basic-trigger');
 	trigger.unbind('click');
-	trigger.click(function(){Animator.init()});
+	trigger.click(Animator.init);
 	trigger.text('Click me to start animating');
-	this.go_on = false;
+	Animator.master_kill = true;
     }
 }
 
 document.ready = function(){
-    $('button.trigger#basic-trigger').click(function(){
-	Animator.init();
-    });
+    $('button.trigger#basic-trigger').click(Animator.init);
 }
