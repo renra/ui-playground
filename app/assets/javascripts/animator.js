229268b1b90d9d9a9d3cfd098012ebd1ccc46723
Jan Gloser <jan.gloser@centrum.cz>
2011-11-27 19:25:41 +0100

    Using smooth rendering, dynamically checking drawing area

--- app/assets/javascripts/animator.js
+++ app/assets/javascripts/animator.js
@@ -1,10 +1,9 @@
-//TODO : add compatibility for other browsers later
 window.requestAnimFrame = (function(){
-    return requestAnimationFrame ||
-	webkitRequestAnimationFrame ||
-	mozRequestAnimationFrame ||
-	oRequestAnimationFrame ||
-	msRequestAnimationFrame ||
+    return window.requestAnimationFrame ||
+	window.webkitRequestAnimationFrame ||
+	window.mozRequestAnimationFrame ||
+	window.oRequestAnimationFrame ||
+	window.msRequestAnimationFrame ||
 	function(callback, element){
 	    window.setTimeout(callback, 1000 / 60)
 	}
@@ -13,34 +12,83 @@ window.requestAnimFrame = (function(){
 
 //TODO : Create instances so that the behaviour is separated and can be used for different objects on the site
 Animator = {
-    animate : function(){
-	console.log('Animating');
+    init : function(){
+	var trigger = $('#basic-trigger');
 	var subject = $('.dancer');
-	for(var i = 0; i < subject.length; i++){
-	    var width = parseInt(subject[i].style.width) || 0;
-	    var height = parseInt(subject[i].style.height) || 0;
-	    var max_x = (width == 0) ? window.innerWidth : (Math.floor(window.innerWidth - width));
-	    var max_y = (height == 0) ? window.innerHeight : (Math.floor(window.innerHeight - height));
-	    
-	    subject[i].style.left = Math.random()*max_x + 'px';
-	    subject[i].style.top = Math.random()*max_y + 'px';
-	    //This works too fast and does not really animate. I guess it works just with pixels
-	    //requestAnimFrame(Animator.animate, subject[i]);
-	}
+	this.origin_x = subject.position().left;
+	this.origin_y = subject.position().top;
+	this.subject = subject[0];
+	
+	this.check_drawing_area();
+	this.x = 0;
+	this.y = 0;
+	this.new_destination();
+	trigger.unbind('click');
+	trigger.click(function(){Animator.kill_animation()});
+	trigger.text('Click me to stop animating');
+	this.go_on = true;
+	this.animate();
     },
 
-    animation_loop : function(){
-	//Can 'this' be used here somehow?
-	var trigger = $('button.trigger#basic-trigger');
-	trigger.attr('onclick', 'Animator.kill_animation();');
-	trigger.text('Click me to stop animating');
-	this.interval_id = setInterval(this.animate, 1000);
+    go_on : true,
+    speed : 0.1,
+    max_x : 0,
+    max_y : 0,
+    
+    animate : function(){
+	if(!Animator.go_on){
+	    return;
+	}
+
+	Animator.check_drawing_area();
+
+	var flag = 0;
+	if( (Math.abs(Animator.dest_x - Animator.x)) > 1 ){
+	    Animator.x += (Animator.dest_x - Animator.x)*Animator.speed;
+	    Animator.subject.style.left = Animator.x + 'px';
+	}
+	else{
+	    flag++;
+	}
+
+	if( (Math.abs(Animator.dest_y - Animator.y)) > 1 ){
+	    Animator.y += (Animator.dest_y - Animator.y)*Animator.speed;
+	    Animator.subject.style.top = Animator.y + 'px';
+	}
+	else{
+	    flag++;
+	}
+
+	if(flag >= 2){
+	    Animator.new_destination();
+	}
+	
+	requestAnimFrame(Animator.animate, Animator.subject);
+    },
+
+    new_destination : function(){
+	Animator.dest_x = Math.random()*Animator.max_x;
+	Animator.dest_y = Math.random()*Animator.max_y;
+    },
+
+    check_drawing_area : function(){
+	var width = parseInt(Animator.subject.style.width) || 0;
+	var height = parseInt(Animator.subject.style.height) || 0;
+	Animator.max_x = Math.floor(window.innerWidth - width);
+	Animator.max_y = Math.floor(window.innerHeight - height);
     },
 
     kill_animation : function(){
 	var trigger = $('button.trigger#basic-trigger');
-	trigger.attr('onclick', 'Animator.animation_loop();');
+	trigger.unbind('click');
+	trigger.click(function(){Animator.init()});
 	trigger.text('Click me to start animating');
-	clearInterval(this.interval_id);	
+	this.go_on = false;
     }
+}
+
+document.ready = function(){
+    $('button.trigger#basic-trigger').click(function(){
+	Animator.init();
+    });
 }
